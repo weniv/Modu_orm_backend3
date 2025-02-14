b233754e3c2d18cf183b8210ec3561e53e7133db
Chinseok Lee <me@pyhub.kr>
2025-02-14 09:12:51 +0900

    health 앱 생성 및 등록

--- pyhub-02/drf-api-01/config/settings.py
+++ pyhub-02/drf-api-01/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "rest_framework",
     "melon",
+    "health",
 ]
 
 MIDDLEWARE = [
--- pyhub-02/drf-api-01/config/urls.py
+++ pyhub-02/drf-api-01/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("melon/", include("melon.urls")),
+    path("health/", include("health.urls")),
 ]

f2da1e5a26a49ce84db816c7afc9549cb6f2531a
Chinseok Lee <me@pyhub.kr>
2025-02-17 14:30:33 +0900

    댓글 목록 View를 구현하고, post detail에서 Ajax 방식으로 댓글목록 로딩

--- pyhub-02/drf-api-01/diary/urls.py
+++ pyhub-02/drf-api-01/diary/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.post_list),
     path("<int:pk>/", views.post_detail),
+    path("<int:post_pk>/comments/", views.comment_list),
 ]
--- pyhub-02/drf-api-01/diary/views.py
+++ pyhub-02/drf-api-01/diary/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from diary.models import Post
+from diary.models import Post, Comment
 
 
 # as_view 함수는 언제 호출되는가 ??
@@ -17,3 +17,16 @@ post_list = PostListView.as_view()
 
 post_detail = DetailView.as_view(model=Post)
 # context_data로서 comment_list 쿼리셋을 추가
+
+
+class CommentListView(ListView):
+    model = Comment
+
+    def get_queryset(self):
+        post_pk = self.kwargs["post_pk"]
+        qs = super().get_queryset()
+        qs = qs.filter(post__pk=post_pk)
+        return qs
+
+
+comment_list = CommentListView.as_view()

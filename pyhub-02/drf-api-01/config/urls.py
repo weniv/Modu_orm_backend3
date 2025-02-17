bbbf42284fb849904849bbf273fcbba006ca0f35
Chinseok Lee <me@pyhub.kr>
2025-02-17 11:42:19 +0900

    diary 앱 생성 및 등록

--- pyhub-02/drf-api-01/config/urls.py
+++ pyhub-02/drf-api-01/config/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
     path("melon/", include("melon.urls")),
     path("health/", include("health.urls")),
+    path("diary/", include("diary.urls")),
     path('api-auth/', include('rest_framework.urls'))
 ]

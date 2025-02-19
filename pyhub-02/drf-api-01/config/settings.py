21f11069e806adadb93cdbe5d56643e0104d7a37
Chinseok Lee <me@pyhub.kr>
2025-02-19 11:22:21 +0900

    django-cors-headers 라이브러리 적용

--- pyhub-02/drf-api-01/config/settings.py
+++ pyhub-02/drf-api-01/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "corsheaders",
     "rest_framework",
     "blog",
     "melon",
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -141,3 +143,11 @@ REST_FRAMEWORK = {
         "blog.renderers.WordcloudRenderer",
     ]
 }
+
+# django-cors-headers
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3001",
+    "http://localhost:3001",
+]

49ef518259c5b6f2e7d0845cdf5030e6df7c1737
Chinseok Lee <me@pyhub.kr>
2025-02-19 11:44:50 +0900

    localhost:3000 으로부터의 로그인/로그아웃 요청을 허용하고, 성공 주소로 사용할 수 있도록 설정 적용

--- pyhub-02/drf-api-01/accounts/views.py
+++ pyhub-02/drf-api-01/accounts/views.py
@@ -4,12 +4,24 @@ from django.contrib.auth.views import LoginView, LogoutView
 login = LoginView.as_view(
     # form_class=LoginForm,
     template_name="diary/form.html",
-    # success_url_allowed_hosts=  # TODO:
+    # 이런 설정은 소스코드에 하드코딩 하지 않습니다 !!!
+    #  - 환경변수로부터의 주입을 권장 !!!
+    success_url_allowed_hosts=[
+        "localhost:3000",
+        "localhost:3001",
+        "127.0.0.1:3000",
+        "127.0.0.1:3001",
+    ],
 )
 
 # ?next= 지정을 추천
 # settings.LOGOUT_REDIRECT_URL = "accounts:login"
 # 이동 주소가 지정되지 않으면 로그아웃 후에, admin 페이지 스타일로 로그아웃 완료 메세지가 보여집니다.
 logout = LogoutView.as_view(
-    # success_url_allowed_hosts=  # TODO:
+    success_url_allowed_hosts=[
+        "localhost:3000",
+        "localhost:3001",
+        "127.0.0.1:3000",
+        "127.0.0.1:3001",
+    ],
 )

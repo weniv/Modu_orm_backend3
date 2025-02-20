5b14e54595ef6d837619b42e6115a5f77fa122ed
Chinseok Lee <me@pyhub.kr>
2025-02-21 00:47:57 +0900

    ASGI 활성화

--- pyhub-02/drf-api-01/config/settings.py
+++ pyhub-02/drf-api-01/config/settings.py
@@ -47,6 +47,7 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -97,6 +98,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 
 # Database

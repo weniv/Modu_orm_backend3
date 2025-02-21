c01ca127f1d339d4ed1b46fb0d3c78413f44806c
Chinseok Lee <me@pyhub.kr>
2025-02-21 15:11:44 +0900

    mynextjs 브라우저 단 fetch 요청에서 세션 인증 활용

--- pyhub-02/mynextjs/pages/blog/index.js
+++ pyhub-02/mynextjs/pages/blog/index.js
@@ -1,11 +1,21 @@
 // [Next.js 프로젝트]
 // pages/blog/index.js
-//  주의: src 디렉토리가 아닙니다.
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+// 웹브라우저에 코드가 다운로드된 후에 수행.
 function WhoamiPage() {
   const [message, setMessage] = useState("no message");
+
+  // 컴포넌트 초기화 시에 1회만 실행.
+  useEffect(() => {
+    // 이 fetch 요청은 브라우저에서 수행됩니다.
+    fetch("http://localhost:8000/blog/whoami/", { credentials: "include" })
+      .then((response) => response.text())
+      .then((responseText) => {
+        setMessage(responseText);
+      });
+  }, []);
 
   return (
     <div>

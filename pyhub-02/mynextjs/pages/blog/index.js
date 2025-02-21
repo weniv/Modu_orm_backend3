f47b6163b7ffe1e761f1e769eac9e2e2ad07fc3c
Chinseok Lee <me@pyhub.kr>
2025-02-21 15:20:34 +0900

    mynextjs 서버 단 fetch 요청에서 세션 인증 활용

--- pyhub-02/mynextjs/pages/blog/index.js
+++ pyhub-02/mynextjs/pages/blog/index.js
@@ -1,30 +1,34 @@
 // [Next.js 프로젝트]
 // pages/blog/index.js
 
-import { useEffect, useState } from "react";
-
-// 웹브라우저에 코드가 다운로드된 후에 수행.
-function WhoamiPage() {
-  const [message, setMessage] = useState("no message");
-
-  // 컴포넌트 초기화 시에 1회만 실행.
-  useEffect(() => {
-    // 이 fetch 요청은 브라우저에서 수행됩니다.
-    fetch("http://localhost:8000/blog/whoami/", { credentials: "include" })
-      .then((response) => response.text())
-      .then((responseText) => {
-        setMessage(responseText);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h2>whoami</h2>
-      <pre>{message}</pre>
-      <hr />
-      <small>by Next.js</small>
-    </div>
-  );
-}
-
-export default WhoamiPage;
+// 요청을 받으면, 응답 생성 시에 서버 단에서 호출
+export async function getServerSideProps(context) {
+    // http://localhost:3000 에서의 쿠키를 API 요청에 활용
+    const headers = {
+      Cookie: context.req.headers.cookie,
+    };
+    console.log("headers: ", headers);
+  
+    const url = "http://localhost:8000/blog/whoami/";
+    const response = await fetch(url, { headers });
+    const responseText = `상태코드: ${response.status}
+  
+  ${await response.text()}`;
+  
+    // props로 전달한 값이 컴포넌트의 속성값으로 주입
+    return { props: { message: responseText } };
+  }
+  
+  // 웹브라우저에 코드가 다운로드된 후에 수행.
+  function WhoamiPage({ message }) {
+    return (
+      <div>
+        <h2>whoami</h2>
+        <pre>{message}</pre>
+        <hr />
+        <small>by Next.js</small>
+      </div>
+    );
+  }
+  
+  export default WhoamiPage;

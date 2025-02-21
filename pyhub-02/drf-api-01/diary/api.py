db7b6c74dbd9417755159a09982835cfed74516d
Chinseok Lee <me@pyhub.kr>
2025-02-21 09:54:14 +0900

    ModelViewSet을 통한 diary.Post REST API 구현

--- pyhub-02/drf-api-01/diary/api.py
+++ pyhub-02/drf-api-01/diary/api.py
@@ -6,10 +6,13 @@ from rest_framework.generics import (
     UpdateAPIView,
     DestroyAPIView,
     ListAPIView,
+    GenericAPIView,
 )
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from diary.models import Post, Comment
 from diary.serializers import PostSerializer, CommentSerializer, PostListSerializer
@@ -24,35 +27,71 @@ from diary.serializers import PostSerializer, CommentSerializer, PostListSeriali
 #     return Response(serializer.errors)
 
 
-class PostListAPIView(ListAPIView):
-    queryset = PostListSerializer.get_optimized_queryset()
-    serializer_class = PostListSerializer
-    permission_classes = [AllowAny]
-
-
-post_list = PostListAPIView.as_view()
-
-
-class PostCreateAPIView(CreateAPIView):
+# 기본 5개의 API Endpoint 생성 + 물론 추가 Endpoint 도 가능 !!!
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
-    # API 별로 권한 설정
-    # permission_classes = [
-    #     # AllowAny,
-    #     IsAuthenticated,
-    # ]
+
+    # permission_classes = [AllowAny]  # 5개 API에 모두 적용
+
+    def get_permissions(self):
+        if self.action == "list":
+            return [AllowAny()]
+        return super().get_permissions()
+
+    def get_queryset(self):
+        if self.action == "list":  # GET
+            return PostListSerializer.get_optimized_queryset()
+        elif self.action == "retrieve":
+            return Post.objects.exclude(status=Post.Status.DELETED)  # 범위
+        elif self.action in ("update", "partial_update"):  # PUT or PATCH
+            return Post.objects.exclude(status=Post.Status.DELETED)  # 범위
+        elif self.action == "destroy":
+            return Post.objects.exclude(status=Post.Status.DELETED)  # 범위
+        return super().get_queryset()
+
+    def get_serializer_class(self):
+        # self.request.method  # "GET"
+        # self.action  # "list", "retrieve", "create", "update", "partial_update", "destroy"
+
+        if self.action == "list":
+            return PostListSerializer
+        return super().get_serializer_class()
 
     def perform_create(self, serializer):
-        # commit인자는 ModelForm만 지원할 뿐
-        # ModelSeralizer에서는 지원하지 않아요.
-        # serializer.save(commit=False)  # XXX
+        serializer.save(user=self.request.user)
 
-        serializer.save(
-            user=self.request.user,
-            # ip=...,
-        )
+    def perform_destroy(self, instance):
+        instance.soft_delete()
 
 
-post_new = PostCreateAPIView.as_view()
+# class PostListAPIView(ListAPIView):
+#     queryset = PostListSerializer.get_optimized_queryset()
+#     serializer_class = PostListSerializer
+#     permission_classes = [AllowAny]
+
+# post_list = PostListAPIView.as_view()
+
+
+# class PostCreateAPIView(CreateAPIView):
+#     serializer_class = PostSerializer
+#     # API 별로 권한 설정
+#     # permission_classes = [
+#     #     # AllowAny,
+#     #     IsAuthenticated,
+#     # ]
+#
+#     def perform_create(self, serializer):
+#         # commit인자는 ModelForm만 지원할 뿐
+#         # ModelSeralizer에서는 지원하지 않아요.
+#         # serializer.save(commit=False)  # XXX
+#
+#         serializer.save(
+#             user=self.request.user,
+#             # ip=...,
+#         )
+
+# post_new = PostCreateAPIView.as_view()
 
 # @api_view(["PUT"])
 # def post_edit(request: Request, pk) -> Response:
@@ -65,26 +104,26 @@ post_new = PostCreateAPIView.as_view()
 #     return Response(serializer.errors)
 
 
-class PostUpdateAPIView(UpdateAPIView):
-    queryset = Post.objects.exclude(status=Post.Status.DELETED)  # 범위
-    serializer_class = PostSerializer
-
-
-post_edit = PostUpdateAPIView.as_view()
-
-
-class PostDestroyAPIView(DestroyAPIView):
-    queryset = Post.objects.exclude(status=Post.Status.DELETED)  # 범위
-
-    # Soft Delete
-    def perform_destroy(self, instance: Post):
-        # instance.content = ""
-        # instance.status = Post.Status.DELETED
-        # instance.save()  # 모든 필드 값을 데이터베이스에 UPDATE 시도
-        instance.soft_delete()
-
-
-post_delete = PostDestroyAPIView.as_view()
+# class PostUpdateAPIView(UpdateAPIView):
+#     queryset = Post.objects.exclude(status=Post.Status.DELETED)  # 범위
+#     serializer_class = PostSerializer
+#
+#
+# post_edit = PostUpdateAPIView.as_view()
+#
+#
+# class PostDestroyAPIView(DestroyAPIView):
+#     queryset = Post.objects.exclude(status=Post.Status.DELETED)  # 범위
+#
+#     # Soft Delete
+#     def perform_destroy(self, instance: Post):
+#         # instance.content = ""
+#         # instance.status = Post.Status.DELETED
+#         # instance.save()  # 모든 필드 값을 데이터베이스에 UPDATE 시도
+#         instance.soft_delete()
+#
+#
+# post_delete = PostDestroyAPIView.as_view()
 
 
 # def comment_list(request, post_pk):
@@ -114,3 +153,35 @@ class CommentListAPIView(ListAPIView):
 
 
 comment_list = CommentListAPIView.as_view()
+
+
+# class PostListCreateAPIView(CreateModelMixin, ListModelMixin, GenericAPIView):
+#
+#     # list
+#     queryset = PostListSerializer.get_optimized_queryset()
+#     # serializer_class = PostListSerializer
+#     permission_classes = [AllowAny]
+#
+#     # create
+#     # serializer_class = PostSerializer
+#
+#     def get_serializer_class(self):
+#         if self.request.method == "GET":
+#             return PostListSerializer
+#         return PostSerializer
+#
+#     def perform_create(self, serializer):
+#         # commit인자는 ModelForm만 지원할 뿐
+#         # ModelSeralizer에서는 지원하지 않아요.
+#         # serializer.save(commit=False)  # XXX
+#
+#         serializer.save(
+#             user=self.request.user,
+#             # ip=...,
+#         )
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
--- pyhub-02/drf-api-01/diary/urls.py
+++ pyhub-02/drf-api-01/diary/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
 from . import api
+from .api import PostViewSet
 
 app_name = "diary"  # URL Reverse 에서의 namespace
 
@@ -13,11 +16,20 @@ urlpatterns = [
     path("<int:post_pk>/comments/new/", views.comment_new, name="comment-new"),
 ]
 
-urlpatterns_api_v1 = [
-    path("posts/", api.post_list, name="post-list"),
-    path("posts/new/", api.post_new, name="post-new"),
-    path("posts/<int:pk>/edit/", api.post_edit, name="post-edit"),
-    path("posts/<int:pk>/delete/", api.post_delete, name="post-delete"),
+# post_list_create_api_view = PostViewSet.as_view({ "get": "list", "post": "create" })  # View Function
+
+router = DefaultRouter()
+router.register("posts", PostViewSet)
+urlpatterns_api_v1 = (
+    router.urls
+)  # 한 번에 2개 Endpoint 생성 (내부에서 5개 action으로 분기)
+
+urlpatterns_api_v1 += [
+    # path("posts/", api.post_list, name="post-list"),  # GET
+    # path("posts/", api.post_new, name="post-new"),  # POST
+    # path("posts/<int:pk>/", api.post_detail, name="post-edit"),  # GET
+    # path("posts/<int:pk>/", api.post_edit, name="post-edit"),  # PUT/PATCH
+    # path("posts/<int:pk>/", api.post_delete, name="post-delete"),  # DELETE
     path("posts/<int:post_pk>/comments/", api.comment_list, name="comment-list"),
 ]
 
